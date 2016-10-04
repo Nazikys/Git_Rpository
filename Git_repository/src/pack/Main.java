5bffdd0291f9389d1d585c7934c57f43fe238013
KongDonkey <qwert111>
2016-10-04 17:00:31 +0300

    first commit

---
+++ Git_repository/src/pack/Main.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		Person person = new Person("anton", 29);
+		System.out.println(person);
+		
+		
+	}
+
+}
